88bd1b01a1d05a2da450170cc0e2c5e31abd28f9
Aaloone <tjtolk@gmail.com>
2017-12-18 20:48:58 +0800

    OLD

--- Day10/进程lock.py
+++ Day10/进程lock.py
@@ -5,12 +5,12 @@ from multiprocessing import Process, Lock
 
 def f(l, i):
     #l.acquire()
-    print('hello world', i)   
+    print('hello world', i)
     #l.release()
 
 
 if __name__ == '__main__':
-    lock = Lock()
+    lock = Lock()   # 保证在屏幕上的打印数据
 
     for num in range(100):
         Process(target=f, args=(lock, num)).start()
--- Day10/进程池.py
+++ Day10/进程池.py
@@ -12,14 +12,15 @@ def Foo(i):
 def Bar(arg):
     print('-->exec done:', arg,os.getpid())
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # 区别主动执行还是当作模块调用
     #freeze_support()
     pool = Pool(processes=3) #允许进程池同时放入5个进程
     print("主进程",os.getpid())
     for i in range(10):
-        pool.apply_async(func=Foo, args=(i,), callback=Bar) #callback=回调
-        #pool.apply(func=Foo, args=(i,)) #串行
-        #pool.apply_async(func=Foo, args=(i,)) #串行
+        # pool.apply_async(func=Foo, args=(i,), callback=Bar(11))  # callback=回调 前面执行完之后，执行 callback 后面的函数,加参数限制性回掉函数，
+        pool.apply_async(func=Foo, args=(i,), callback= Bar)  # callback=回调 前面执行完之后，执行 callback 后面的函数，
+        #pool.apply(func=Foo, args=(i,)) # 串行  一个一个执行，跟每个后面加了  .join() 一样
+        #pool.apply_async(func=Foo, args=(i,)) # 并行
     print('end')
     pool.close()
     pool.join() #进程池中进程执行完毕后再关闭，如果注释，那么程序直接关闭。.join()
